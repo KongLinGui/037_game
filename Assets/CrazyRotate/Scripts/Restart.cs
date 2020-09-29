0cf12c9babfe6be26e18f9f6b9d00404d434f8bf
KongLinGui <18737010615@163.com>
2020-09-29 13:17:42 +0800

    Crazy Rotate 2.01 一个小游戏的案例

---
+++ Assets/CrazyRotate/Scripts/Restart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 重新开始
+/// </summary>
+public class Restart : MonoBehaviour 
+{
+	void OnMouseDown()
+	{
+		GameController.instance.Restart();
+	}
+}
---
+++ Assets/CrazyRotate/Scripts/SoundManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 声音管理脚本
+/// </summary>
+public class SoundManager : Singleton<SoundManager>
+{
+	public AudioClip clickSound;
+	public AudioClip scoreSound;
+	public AudioClip gameOverSound;
+
+	public AudioSource mySource;
+
+	protected override void Initialize()
+	{
+
+	}
+
+	public void EnableSound()
+	{
+		PlayClickSound();
+		mySource.volume = 1f;
+		GameManager.GetInstance().soundOn = true;
+	}
+
+	public void DisableSound()
+	{
+		mySource.volume = 0f;
+		GameManager.GetInstance().soundOn = false;
+	}
+	
+    /// <summary>
+    /// 游戏点击声音
+    /// </summary>
+	public void PlayClickSound()
+	{
+		mySource.clip = clickSound;
+		MakeSound();
+	}
+
+    /// <summary>
+    /// 游戏得分声音
+    /// </summary>
+	public void PlayScoreSound()
+	{
+		mySource.clip = scoreSound;
+		mySource.Play();
+	}
+
+    /// <summary>
+    /// 游戏结束声音
+    /// </summary>
+	public void PlayGameOverSound()
+	{
+		mySource.clip = gameOverSound;
+		mySource.Play();
+	}
+
+	void MakeSound() 
+	{
+		mySource.Play();
+	}
+}
